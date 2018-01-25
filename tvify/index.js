914e19d1ce6b14d71dfd0d702ff3ccbbff7f5633
lfrFlores <luis_naiby_flores@outlook.com>
2018-01-25 11:42:33 -0500

    jQueryObject

--- tvify/index.js
+++ tvify/index.js
@@ -1,8 +1,22 @@
 
 $(function(){
-	var subHeader = $('h1 + h2') //todos los h2 hermanos de h1
-	console.log(subHeader)
+	 var h1= $('#app-header').find('h1') //buscar h1 dentro de app-header
+	 /*$('#app-header').has('h1')//todos los app-header que tengan h1 dentro
+ 	 $('#app-header').has('.title')//todos los app-header que tengan la clase title dentro
+	  $('#app-header').not('.title')//todos los app-header que no tengan h1 dentro
+	 
+	 $('p').filter('.text') //<p class="text"></p>
+	
 
-	var headings = $('h1, h2');
-	console.log(headings);
+	el siguiente jQueryObject devuelve todos los p que tengan la clase text ademas que tengan a y que me devuelva el segundo
+	*/ 
+	 var ps= $('p')
+		.filter('.text')
+		.has('a')
+		.eq(1) //.first() es para es primero luego eq(subIndice) para los demás 
+
+	
+
+
+	 console.log(h1)
 })
