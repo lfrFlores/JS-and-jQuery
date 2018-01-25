532fb99c51c6f6a579c63c1d98cb0f6d73d331e9
lfrFlores <luis_naiby_flores@outlook.com>
2018-01-25 10:34:49 -0500

    update index.html

--- tvify/index.js
+++ tvify/index.js
@@ -1,9 +1,3 @@
-/*que pasa se utilizamos jquery con prototipe?
-cual cargamos primero?
-entonces lo que podemos hacer es 
-*/
-$.noConflict();
-jQuery(document).ready(function($){
-	$ //jQuery
+
+$(function(){
 })
-$ //Prototype
