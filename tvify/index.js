00138e2b90c4afa548d3082007ee3b44f7364dac
lfrFlores <luis_naiby_flores@outlook.com>
2018-01-25 09:52:23 -0500

    Home

---
+++ tvify/index.js
@@ -0,0 +1,28 @@
+/*var alerta= function(mensaje){
+	alert(mensaje)
+}
+
+alerta("Hola Luis Flores")
+*/
+
+
+/*
+1° Forma
+$(document).ready(function () {
+    alert( 'ready!' );
+});
+2° Forma
+$().ready(function () {
+    alert( 'ready!' );
+});
+
+3° Forma*/
+$(function () {
+    alert( 'ready!' );
+});
+
+
+window.onload= function () {
+	alert('loaded')
+}
+
