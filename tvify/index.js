1ff1c14ae003c6e4be16a6532f793d921a3dae98
lfrFlores <luis_naiby_flores@outlook.com>
2018-01-30 11:28:34 -0500

    modifying the DOM

--- tvify/index.js
+++ tvify/index.js
@@ -1,5 +1,8 @@
 $(function(){
-	$('h1').css({
-		'font-size': '70px'
-	})
+	// $('header#app-header')
+	// .append($('<p>',{html:'Me acaban de crear'}))
+
+	$('<p>',{html:'Me acaban de crear'})
+	.appendTo($('header#app-header'))
+
 })
